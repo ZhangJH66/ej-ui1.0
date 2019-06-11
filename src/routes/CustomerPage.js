d7a84cd35c4a32b93a4aa650518746a7844b63c7
Dong <1010884871@qq.com>
2019-06-11 18:40:30 +0800

    孔令东，提交：顾客的删除，批量删除

--- src/routes/CustomerPage.js
+++ src/routes/CustomerPage.js
@@ -2,7 +2,7 @@ import React from 'react';
 // 引入css进行页面美化
 import styles from './CustomerPage.css'
 // 导入组件
-import {Button, Table} from 'antd'
+import {Modal,Button, Table,message} from 'antd'
 import axios from '../utils/axios'
 
 // 组件类必须要继承React.Component，是一个模块，顾客管理子功能
@@ -11,6 +11,7 @@ class CustomerPage extends React.Component {
   constructor(){
     super();
     this.state = {
+      ids:[], // 批量删除的时候保存的id
       list:[],
       loading:false
     }
@@ -32,7 +33,42 @@ class CustomerPage extends React.Component {
       this.setState({loading:false});
     })
   }
+  // 批量删除
+  handleBatchDelete(){
+    Modal.confirm({
+      title: '确定删除这些记录吗?',
+      content: '删除后数据将无法恢复',
+      onOk:() => {
+        axios.post("/customer/batchDelete",{ids:this.state.ids})
+        .then((result)=>{
+          //批量删除后重载数据
+          message.success(result.statusText)
+          this.reloadData();
+        })
+      }
+    });
+  }
 
+  // 单个删除
+  handleDelete(id){
+    Modal.confirm({
+      title: '确定删除这条记录吗?',
+      content: '删除后数据将无法恢复',
+      onOk:() => {
+        // 删除操作
+        axios.get("/customer/deleteById",{
+          params:{
+            id:id
+          }
+        })
+        .then((result)=>{
+          // 删除成功后提醒消息，并且重载数据
+          message.success(result.statusText);
+          this.reloadData();
+        })
+      }
+    });
+  }
 
   // 组件类务必要重写的方法，表示页面渲染
   render(){
@@ -45,20 +81,27 @@ class CustomerPage extends React.Component {
       dataIndex:'telephone'
     },{
       title:'状态',
+      align:"center",
       dataIndex:'status'
     },{
       title:'操作',
-      render:function(){
+      width:120,
+      align:"center",
+      render:(text,record)=>{
         return (
           <div>
-            <Button type='link' size="small">删除</Button>
+            <Button type='link' size="small" onClick={this.handleDelete.bind(this,record.id)}>删除</Button>
+            <Button type='link' size="small">修改</Button>
           </div>
         )
       }
     }]
     const rowSelection = {
       onChange: (selectedRowKeys, selectedRows) => {
-        console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows);
+        // 当用户操作复选按钮的时候，将值获取到并且保存到state中
+        this.setState({
+          ids:selectedRowKeys
+        })
       },
       getCheckboxProps: record => ({
         disabled: record.name === 'Disabled User', // Column configuration not to be checked
@@ -72,7 +115,7 @@ class CustomerPage extends React.Component {
         <div className={styles.title}>顾客管理</div>
         <div className={styles.btns}>
           <Button>添加</Button> &nbsp;
-          <Button>批量删除</Button> &nbsp;
+          <Button onClick={this.handleBatchDelete.bind(this)}>批量删除</Button> &nbsp;
           <Button type="link">导出</Button>
         </div>
         <Table 
