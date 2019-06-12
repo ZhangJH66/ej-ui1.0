a914927239a502f93ce157d62c41dc5e08f64aba
Gao <1799551632@qq.com>
2019-06-12 14:32:34 +0800

    高大唤，提交category和comment的添加信息功能

---
+++ src/routes/CommentForm.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import {Form,Modal,Input,Radio} from 'antd'
+
+class CommentForm extends React.Component {
+
+  render(){
+    // 父组件传递给子组件值
+    const { visible, onCancel, onCreate, form } = this.props;
+    const { getFieldDecorator } = form;
+    return (
+      <Modal
+          visible={visible}
+          title="添加评论信息"
+          okText="提交"
+          onCancel={onCancel}
+          onOk={onCreate}
+        >
+          <Form layout="vertical">
+            <Form.Item label="评论内容">
+              {getFieldDecorator('content', {
+                rules: [{ required: true, message: '请输入评论内容!' }],
+              })(<Input />)}
+            </Form.Item>
+            <Form.Item label="评论时间">
+              {getFieldDecorator('comment_time', {
+                rules: [{ required: true, message: '请输入评论时间!' }],
+              })(<Input />)}
+            </Form.Item>
+            
+           
+          </Form>
+        </Modal>
+    );
+  }
+}
+export default Form.create()(CommentForm);
